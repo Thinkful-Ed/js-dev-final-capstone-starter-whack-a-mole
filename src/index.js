76511873290bb67159dd2a539c1758d45f6ae73f
Mary Gail Ang <maryg@indeed.com>
2024-05-28 19:53:04 +0900

    Changes for US-04 complete

--- src/index.js
+++ src/index.js
@@ -163,7 +163,8 @@ function toggleVisibility(hole){
 */
 function updateScore() {
   // TODO: Write your code here
-
+  points++;
+  score.textContent = points;
   return points;
 }
 
@@ -176,8 +177,8 @@ function updateScore() {
 */
 function clearScore() {
   // TODO: Write your code here
-  // points = 0;
-  // score.textContent = points;
+   points = 0;
+   score.textContent = points;
   return points;
 }
 
@@ -215,7 +216,7 @@ function startTimer() {
 */
 function whack(event) {
   // TODO: Write your code here.
-  // call updateScore()
+  updateScore()
   return points;
 }
 
@@ -226,7 +227,7 @@ function whack(event) {
 */
 function setEventListeners(){
   // TODO: Write your code here
-
+  moles.forEach( mole => mole.addEventListener("click",whack));
   return moles;
 }
 
