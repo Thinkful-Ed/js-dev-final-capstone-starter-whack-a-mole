4b2adaff2932dd6e27edb88c4dd213f7cea4e57e
Mary Gail Ang <maryg@indeed.com>
2024-05-28 18:21:48 +0900

    Changes for US-02

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -41,7 +41,13 @@ function randomInteger(min, max) {
  */
 function setDelay(difficulty) {
   // TODO: Write your code here.
-  
+  if (difficulty === "easy"){
+    return 1500
+  } else if (difficulty === "normal"){
+    return 1000
+  } else if (difficulty === "hard"){
+    return randomInteger(600, 1200)
+  }
 }
 
 /**
@@ -60,7 +66,17 @@ function setDelay(difficulty) {
  */
 function chooseHole(holes) {
   // TODO: Write your code here.
-
+  // 1. Generate a random integer from 0 to 8 and assign it to an index variable.
+  const index = randomInteger(0, 8);
+  // 2. Get a random hole with the random index (e.g., const hole = holes[index]).
+  const hole = holes[index];
+  // 3. if hole === lastHole, then call chooseHole(holes) again because you don't want to return the same hole.
+  if (hole === lastHole){
+    chooseHole(holes);
+  }
+  // 4. if hole is not the same as the lastHole, then keep track of it (lastHole = hole) and return the hole.
+  lastHole = hole;
+  return hole;
 }
 
 /**
